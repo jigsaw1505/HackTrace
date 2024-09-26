ca150b3d03c64858b88d8438d6822b9405c398cd
Jigar R Rathod <136077334+jigsaw1505@users.noreply.github.com>
2024-09-26 21:52:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,6 +42,81 @@ else
 fi
 sudo apt-get install rkhunter
 sudo apt install htop
-sudo apt-get update
 sudo apt-get install bc
+if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
+    INSTALL_DIR="$PREFIX/usr/share/doc/Hacktrace"
+    BIN_DIR="$PREFIX/bin/"
+    BASH_PATH="$PREFIX/bin/bash"
+    TERMUX=true
 
+    pkg install -y git python2
+elif [ "$(uname)" = "Darwin" ]; then
+    INSTALL_DIR="/usr/local/Hacktrace"
+    BIN_DIR="/usr/local/bin/"
+    BASH_PATH="/bin/bash"
+    TERMUX=false
+else
+    INSTALL_DIR="$HOME/.Hacktrace"
+    BIN_DIR="/usr/local/bin/"
+    BASH_PATH="/bin/bash"
+    TERMUX=false
+
+fi
+
+echo "[✔] Checking directories...";
+if [ -d "$INSTALL_DIR" ]; then
+    echo "[◉] A directory Hacktrace was found! Do you want to replace it? [Y/n]:" ;
+    read -r mama
+    if [ "$mama" = "y" ]; then
+        if [ "$TERMUX" = true ]; then
+            rm -rf "$INSTALL_DIR"
+            rm "$BIN_DIR/Hacktrace*"
+        else
+            sudo rm -rf "$INSTALL_DIR"
+            sudo rm "$BIN_DIR/Hacktrace*"
+        fi
+    else
+        echo "[✘] If you want to install you must remove previous installations [✘] ";
+        echo "[✘] Installation failed! [✘] ";
+        exit
+    fi
+fi
+echo "[✔] Cleaning up old directories...";
+if [ -d "$ETC_DIR/Manisso" ]; then
+    echo "$DIR_FOUND_TEXT"
+    if [ "$TERMUX" = true ]; then
+        rm -rf "$ETC_DIR/Manisso"
+    else
+        sudo rm -rf "$ETC_DIR/Manisso"
+    fi
+fi
+
+echo "[✔] Installing ...";
+echo "";
+git clone --depth=1 https://github.com/jigsaw1505/HackTrace "$INSTALL_DIR";
+echo "#!$BASH_PATH
+bash $INSTALL_DIR/main.sh" "${1+"$@"}" > "$INSTALL_DIR/Hacktrace";
+chmod +x "$INSTALL_DIR/Hacktrace";
+if [ "$TERMUX" = true ]; then
+    cp "$INSTALL_DIR/Hacktrace" "$BIN_DIR"
+    cp "$INSTALL_DIR/Hacktrace.cfg" "$BIN_DIR"
+else
+    sudo cp "$INSTALL_DIR/Hacktrace" "$BIN_DIR"
+    sudo cp "$INSTALL_DIR/Hacktrace.cfg" "$BIN_DIR"
+fi
+rm "$INSTALL_DIR/Hacktrace";
+
+
+if [ -d "$INSTALL_DIR" ] ;
+then
+    echo "";
+    echo "[✔] Tool installed successfully! [✔]";
+    echo "";
+    echo "[✔]====================================================================[✔]";
+    echo "[✔]      All is done!! You can execute tool by typing Hacktrace !       [✔]";
+    echo "[✔]====================================================================[✔]";
+    echo "";
+else
+    echo "[✘] Installation failed! [✘] ";
+    exit
+fi
